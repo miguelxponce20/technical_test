4de46b5ec3ea73bccd004d008c3e82c537b48e9a
Miguel <miguelxponce20@gmail.com>
2025-05-20 23:08:29 -0400

    Modelo para almacenar datos de la API

--- uf/models.py
+++ uf/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Creamos el modelo UF para almacenar los datos de la API 
 class UF(models.Model):
     date = models.DateField(unique=True)
     value = models.FloatField()
