99b98f4dadf7fe56311c926f06c95bdb7c9cf39d
Miguel <miguelxponce20@gmail.com>
2025-05-20 23:09:21 -0400

    Serializador para la clase UF

--- uf/serialiazers.py
+++ uf/serialiazers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import UF
 
+# Establece el serializador para la clase UF 
 class UFSerializer(serializers.ModelSerializer):
     class Meta:
         model = UF
